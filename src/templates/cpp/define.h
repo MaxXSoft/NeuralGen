6759e423b0adbd8d659d7b54c76d4ad0d075d87a
MaxXSoft <x@MaxXSoft.net>
2021-05-19 15:22:42 +0800

    (define) added some details

--- src/templates/cpp/define.h
+++ src/templates/cpp/define.h
@@ -1,9 +1,19 @@
 #ifndef NEURALGEN_DEFINE_H_
 #define NEURALGEN_DEFINE_H_
 
-#include <cassert>
-#include <cmath>
-#include <cstddef>
+#include <algorithm>    // max pooling
+#include <cassert>      // GetIndex
+#include <cmath>        // ActFuncs
+#include <cstddef>      // size_t
+#include <cstdint>      // module file struct
+#include <cstring>      // main
+#include <fstream>      // main
+#include <iostream>     // main
+#include <memory>       // main
+#include <stdexcept>    // main
+#include <string_view>  // main
+#include <utility>      // main
+#include <vector>       // main
 
 #define CONCAT_IMPL(x, y) x##y
 #define CONCAT(x, y) CONCAT_IMPL(x, y)
@@ -12,6 +22,9 @@
 #define CONV_3D(id) CONCAT(Conv3D_, id)
 #define POOLING(id) CONCAT(Pooling_, id)
 #define FULL_CONN(id) CONCAT(FullConn_, id)
+
+#define DECL_LAYER(type, id) \
+  void type(id)(float *in, float *out, float *weight, float *bias)
 
 inline size_t GetIndex(size_t x, size_t y, size_t channel, size_t width,
                        size_t height, size_t depth) {
