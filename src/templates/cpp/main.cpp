e6411bfb44c6ad2b2696e21f41ac4db7fceafbf4
MaxXSoft <x@MaxXSoft.net>
2021-05-19 15:23:07 +0800

    (main) added file 'main.cpp'

---
+++ src/templates/cpp/main.cpp
@@ -0,0 +1,123 @@
+#ifndef GENERATED
+#include "define.h"
+#endif  // GENERATED
+
+namespace {
+
+/*
+  Model File Format (field: bytes):
+
+  MAGIC_NUMBER:       4
+  LAYER_NUM:          4
+  LAYERn_WEIGHT_SIZE: 4
+  LAYERn_BIAS_SIZE:   4
+  LAYERn_WEIGHT_DATA: LAYERn_WEIGHT_SIZE
+  LAYERn_BIAS_DATA:   LAYERn_BIAS_SIZE
+*/
+
+// pointer to float array
+using FloatArr = std::unique_ptr<float[]>;
+
+// model data (vector of (weight, bias) pairs)
+using ModelData = std::vector<std::pair<FloatArr, FloatArr>>;
+
+// magic number: 1 go ge cal => yi gou ji calc => yi gou ji suan
+constexpr uint32_t kModFileMagicNum = 0x1909eca1;
+
+struct ModelFileHeader {
+  uint32_t magic;
+  uint32_t layer_num;
+};
+
+struct ModelLayerHeader {
+  uint32_t weight_size;
+  uint32_t bias_size;
+};
+
+// open file or fail
+void OpenFile(std::ifstream &ifs, std::string_view file) {
+  ifs.open(file, std::ios::binary);
+  if (!ifs) {
+    throw std::runtime_error("Failed to open file!");
+  }
+}
+
+// read model from file
+ModelData ReadModel(std::istream &is) {
+  // read file header
+  ModelFileHeader mfh;
+  is.read(reinterpret_cast<char *>(&mfh), sizeof(ModelFileHeader));
+  if (mfh.magic != kModFileMagicNum) {
+    throw std::runtime_error("Invalid model file, magic number mismatch!");
+  }
+  // read layers
+  ModelLayerHeader mlh;
+  ModelData model;
+  for (size_t i = 0; i < mfh.layer_num; ++i) {
+    is.read(reinterpret_cast<char *>(&mlh), sizeof(ModelLayerHeader));
+    auto weight = std::make_unique<float[]>(mlh.weight_size);
+    auto bias = std::make_unique<float[]>(mlh.bias_size);
+    is.read(reinterpret_cast<char *>(weight.get()), mlh.weight_size);
+    is.read(reinterpret_cast<char *>(bias.get()), mlh.bias_size);
+    model.push_back({std::move(weight), std::move(bias)});
+  }
+  return model;
+}
+
+// read input from file
+FloatArr ReadInput(std::istream &is) {
+  // read input
+  std::vector<float> arr;
+  while (!is.fail() && !is.eof()) {
+    uint8_t cur;
+    is.read(reinterpret_cast<char *>(&cur), 1);
+    arr.push_back(static_cast<float>(cur));
+  }
+  if (!is) throw std::runtime_error("File error!");
+  // copy to float array
+  auto input = std::make_unique<float[]>(arr.size());
+  std::memcpy(input.get(), arr.data(), arr.size());
+  return input;
+}
+
+// infer
+FloatArr Infer(const ModelData &model, const FloatArr &input) {
+  // TODO
+}
+
+// dump output to stderr
+void DumpOutput(const FloatArr &output) {
+  // TODO
+}
+
+// get the index of the maximum output
+size_t GetMaxIndex(const FloatArr &output) {
+  // TODO
+}
+
+}  // namespace
+
+int main(int argc, const char *argv[]) {
+  // check & parse command line arguments
+  if (argc < 3) {
+    std::cerr << "Usage: " << argv[0] << " MODEL INPUT" << std::endl;
+    return 1;
+  }
+  std::string_view mod_file = argv[1];
+  std::string_view input_file = argv[2];
+
+  // read model data
+  std::ifstream ifs;
+  OpenFile(ifs, mod_file);
+  auto model = ReadModel(ifs);
+
+  // read input
+  OpenFile(ifs, input_file);
+  auto input = ReadInput(ifs);
+
+  // infer
+  auto output = Infer(model, input);
+  DumpOutput(output);
+  std::cout << GetMaxIndex(output) << std::endl;
+  return 0;
+}
