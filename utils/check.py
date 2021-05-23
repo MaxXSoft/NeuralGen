da3cedfb354a6cc8800fcd35caa72eb76d7527ef
MaxXSoft <x@MaxXSoft.net>
2021-05-23 10:50:15 +0800

    (utils/check) added network checker

---
+++ utils/check.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+from os import listdir, path
+from sys import argv
+from typing import List, Tuple, Any, Iterator
+from subprocess import check_output, DEVNULL, CalledProcessError
+from resource import getrusage, RUSAGE_CHILDREN
+
+
+def chunks(l: List[Any], n: int) -> Iterator[List[Any]]:
+  for i in range(0, len(l), n):
+    yield l[i:i + n]
+
+
+def check_case(network: str, model: str, files: List[str]) -> int:
+  expected = [i[:-4].split('-')[-1] for i in files]
+  out = check_output([network, model] + files, stderr=DEVNULL)
+  out = out.decode('utf-8').strip().split('\n')
+  correct = 0
+  for (x, y) in zip(expected, out):
+    if x == y:
+      correct += 1
+  return correct
+
+
+def check(network: str, model: str, test_dir: str) -> Tuple[int, int]:
+  total = 0
+  correct = 0
+  for c in chunks(listdir(test_dir), 50):
+    total += len(c)
+    correct += check_case(network, model,
+                          list(map(lambda x: path.join(test_dir, x), c)))
+  return correct, total
+
+
+if __name__ == '__main__':
+  if len(argv) < 4:
+    print(f'Usage: {argv[0]} NETWORK MODEL TEST_DIR')
+    exit(1)
+  try:
+    c, t = check(argv[1], argv[2], argv[3])
+    print(f'Correct/Total: {c}/{t}')
+    print(f'Correct Rate: {c * 100 / t}%')
+    print(f'Total User Time: {getrusage(RUSAGE_CHILDREN).ru_utime * 1000:.2f}ms')
+  except CalledProcessError:
+    print('Failed to run network!')
+    exit(1)
